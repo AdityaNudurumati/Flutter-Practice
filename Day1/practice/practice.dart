ee73225316e94fe08fa8fce8c4119f3f7e9c79c9
Aditya <71510713+AdityaNudurumati@users.noreply.github.com>
2026-06-15 22:40:25 +0530

    Day1 practice

---
+++ Day1/practice/practice.dart
@@ -0,0 +1,15 @@
+void main(){
+  var x = 1;
+  x = 3;
+  print(x);
+  int y = 2;
+  y = 1;
+  print(y);
+  num z = -1;
+  z = 2.3;
+  print(z);
+  dynamic xx = 'sita';
+  xx = 2.0;
+  Object yy = 'xy';
+  yy = 21;
+}
